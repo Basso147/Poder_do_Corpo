9e81697a8f5b3f173ad3899725a33bb82daa6e0a
Basso147 <joao.adb@aluno.ifsc.edu.br>
2024-08-26 17:26:24 -0300

    a

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -13,5 +13,16 @@ export default{
       {text: "Custos e Orçamento", link: '/Custos.md'},
       {text: "Considerações Finais", link: '/Consideracoes_finais.md'},
     ],
+    sidebar: [
+      {text: "Introdução", link: '/introducao.md'},
+      {text: "Descrição", link: '/Descricao.md'},
+      {text: "Principais Recursos e Funcionalidades", link: '/Principais_Recursos.md'},
+      {text: "Diagrama de Casos de Uso", link: '/Casos_de_uso.md'},
+      {text: "Diagrama de Classe", link: '/Classe.md'},
+      {text: "Cronograma e Entrega", link: '/Cronograma.md'},
+      {text: "Riscos e Mitigação", link: '/Riscos.md'},
+      {text: "Custos e Orçamento", link: '/Custos.md'},
+      {text: "Considerações Finais", link: '/Consideracoes_finais.md'},
+    ],
   },
 }
